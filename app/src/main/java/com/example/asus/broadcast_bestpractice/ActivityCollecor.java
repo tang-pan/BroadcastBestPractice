c3e3063df7d5ca5c3602cb57339a22a99216fe3c
tang-pan <1044639436@qq.com>
2019-01-09 15:36:32 +0800

    First commit.

---
+++ app/src/main/java/com/example/asus/broadcast_bestpractice/ActivityCollecor.java
@@ -0,0 +1,32 @@
+package com.example.asus.broadcast_bestpractice;
+
+import android.app.Activity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by asus on 2019/1/9.
+ */
+
+public class ActivityCollecor {
+
+    public static List<Activity> activities = new ArrayList<>();
+
+    public static void addActivity(Activity activity){
+        activities.add(activity);
+    }
+
+    public static void removeActivity(Activity activity){
+        activities.remove(activity);
+    }
+
+    public static void finishAll(){
+        for (Activity activity : activities){
+            if (!activity.isFinishing()){
+                activity.finish();
+            }
+        }
+    }
+
+}
